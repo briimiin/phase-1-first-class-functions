43f220ab253c351bb7a91e26abd77fd3bc912b3e
briimiin <kipkorirbrimin005@gmail.com>
2024-03-21 00:49:17 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("This is a named function");
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function");
+    }
+}
+receivesAFunction(() => console.log("Callback function called"));
+const namedFunc = returnsANamedFunction();
+namedFunc();
+const anonFunc = returnsAnAnonymousFunction();
+anonFunc();
